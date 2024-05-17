a8e17bf28684104fcc50d71353443ea4b03aeb0d
Arif Mohammed <arifmohammed.technical@gmail.com>
2024-05-17 08:45:37 -0500

    Premium -> Standard

--- aks/main.tf
+++ aks/main.tf
@@ -44,7 +44,7 @@ resource "azurerm_container_registry" "aks" {
   name                = var.acr_name
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  sku                 = "Premium" # use Premium for geo replication
+  sku                 = "Standard" # use Premium for geo replication
   admin_enabled       = false
   /*georeplications {
     location                = var.acr_geo_replicaton_location_1 # cannot be same location as ACR location
